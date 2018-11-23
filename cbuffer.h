907dddff9fb6fe5a00a07228ead2b89b0a8d60e3
Julix8MS <44966040+Julix8MS@users.noreply.github.com>
2018-11-23 11:25:11 +0100

    Create cbuffer.h

---
+++ cbuffer.h
@@ -0,0 +1,5 @@
+// This is a Linux-only speed update for cscreen(COMING SOON FOR WINDOWS)
+// The update includes better screen performance and control and a fast file
+// buffer controller for writing and reading files. Finally, it includes
+// bitmap writing and reading allowing to make bitmaps and screenshots of
+// the window.
